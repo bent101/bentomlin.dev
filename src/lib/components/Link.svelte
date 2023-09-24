a212e45f26a467e95ff17791f39d6f5d1832686d
Ben Tomlin <bentomlin101@gmail.com>
2023-09-24 07:25:44 -0700

    add tooltips

---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import Icon from "@iconify/svelte";
+
+	export let href: string;
+	export let icon: string;
+	export let tooltip: string;
+</script>
+
+<a
+	aria-label={tooltip}
+	target="_blank"
+	{href}
+	class="group relative h-10 w-10 rounded-full p-1 opacity-80 transition-all duration-200 hover:bg-white/10 hover:opacity-100 hover:scale-105"
+>
+	<Icon class="h-full w-full" {icon} />
+	<div
+		class="pointer-events-none absolute -inset-x-96 top-full mx-auto mt-2 w-max origin-top rounded-md bg-white px-2 py-1 text-sm font-semibold text-navy opacity-0 transition-all duration-200 scale-75 group-hover:opacity-100 group-hover:scale-100"
+	>
+		{tooltip}
+	</div>
+</a>
--- src/lib/components/Links.svelte
+++ src/lib/components/Links.svelte
@@ -1,32 +1,24 @@
 <script lang="ts">
 	import Icon from "@iconify/svelte";
+	import Link from "./Link.svelte";
 </script>
 
 <div
 	class="fixed inset-x-0 top-4 z-50 mx-auto flex w-max rounded-full bg-sky-blue p-1 text-white backdrop-blur-md"
 >
-	<a
-		aria-label="Ben's Linkedin"
-		target="_blank"
+	<Link
 		href="https://www.linkedin.com/in/ben-tomlin-46b788275/"
-		class="h-10 w-10 rounded-full p-1 opacity-80 transition-all duration-200 hover:bg-white/10 hover:opacity-100 hover:scale-105"
-	>
-		<Icon class="h-full w-full" icon="mdi:linkedin" />
-	</a>
-	<a
-		aria-label="Send Ben an email"
-		target="_blank"
+		icon="mdi:linkedin"
+		tooltip="View my Linkedin"
+	/>
+	<Link
 		href="mailto:btomlin@ucsd.edu"
-		class="h-10 w-10 rounded-full p-1 opacity-80 transition-all duration-200 hover:bg-white/10 hover:opacity-100 hover:scale-105"
-	>
-		<Icon class="h-full w-full" icon="mdi:email" />
-	</a>
-	<a
-		aria-label="Ben's GitHub"
-		target="_blank"
+		icon="mdi:email"
+		tooltip="Email me (btomlin@ucsd.edu)"
+	/>
+	<Link
 		href="https://github.com/bent101"
-		class="h-10 w-10 rounded-full p-1 opacity-80 transition-all duration-200 hover:bg-white/10 hover:opacity-100 hover:scale-105"
-	>
-		<Icon class="h-full w-full" icon="mdi:github" />
-	</a>
+		icon="mdi:github"
+		tooltip="Explore my GitHub"
+	/>
 </div>
